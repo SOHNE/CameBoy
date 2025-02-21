b66796b735e99173c6ca4322b243dd2088b6b221
Leandro Peres <leandroperes@protonmail.com>
2025-02-21 09:09:06 -0300

    Add SDL2 test

---
+++ src/main.c
@@ -0,0 +1,76 @@
+#include <stdbool.h>
+#include "SDL2/SDL.h"
+
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 144
+#define SCALE_FACTOR 4
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    // Create window
+    window = SDL_CreateWindow("Scaled Pixel Display",
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED,
+                            SCREEN_WIDTH * SCALE_FACTOR,
+                            SCREEN_HEIGHT * SCALE_FACTOR,
+                            SDL_WINDOW_SHOWN);
+    
+    if (window == NULL) {
+        SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    // Create renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        SDL_Log("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    
+    // Set render scale
+    SDL_RenderSetScale(renderer, SCALE_FACTOR, SCALE_FACTOR);
+    
+    // Main loop flag
+    bool quit = false;
+    
+    // Event handler
+    SDL_Event e;
+    
+    // Main loop
+    while (!quit) {
+        // Handle events
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+        
+        // Clear screen
+        SDL_SetRenderDrawColor(renderer, 155, 188, 15, 255);
+        SDL_RenderClear(renderer);
+        
+        // Draw a sample pixel at (80, 72) - center of screen
+        SDL_SetRenderDrawColor(renderer, 15, 56, 15, 255);
+        SDL_RenderDrawPoint(renderer, 80, 72);
+        
+        // Update screen
+        SDL_RenderPresent(renderer);
+    }
+    
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
