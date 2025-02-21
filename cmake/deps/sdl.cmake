af3d06029ade230fabedde10ec194bb2ba48dbaa
Leandro Peres <leandroperes@protonmail.com>
2025-02-21 09:28:42 -0300

    Add tries to get system SDL first

--- cmake/deps/sdl.cmake
+++ cmake/deps/sdl.cmake
@@ -1,28 +1,35 @@
 if(NOT DEFINED EMSCRIPTEN)
-  string(TIMESTAMP BEFORE "%s")
-  CPMAddPackage(
+  find_package(SDL2 QUIET)
+
+  if(NOT SDL2_FOUND)
+    message(STATUS "System SDL2 not found, fetching using CPM...")
+    string(TIMESTAMP BEFORE "%s")
+
+    CPMAddPackage(
       NAME SDL
       GITHUB_REPOSITORY libsdl-org/SDL
       GIT_TAG release-2.32.0
       OPTIONS
-      "SDL_EXAMPLES OFF"
-      "SDL_SHARED OFF"
-      "SDL_STATIC ON"
-      "SDL_TESTS OFF"
-  )
-
-  if(SDL_ADDED)
-    list(APPEND GBE_DEPS_LINK
-      $<TARGET_NAME_IF_EXISTS:SDL2::SDL2main>
-      $<IF:$<TARGET_EXISTS:SDL2::SDL2>,SDL2::SDL2,SDL2::SDL2-static>
+        "SDL_EXAMPLES OFF"
+        "SDL_SHARED OFF"
+        "SDL_STATIC ON"
+        "SDL_TESTS OFF"
     )
+
+    if(NOT SDL_ADDED)
+      message(FATAL_ERROR "Failed to add SDL2 package")
+    endif()
 
     string(TIMESTAMP AFTER "%s")
     math(EXPR DELTASDL "${AFTER} - ${BEFORE}")
-    message(STATUS "SDL2 TIME: ${DELTASDL}s")
+    message(STATUS "SDL2 built from source in ${DELTASDL}s")
   else()
-    message(FATAL_ERROR "Failed to add SDL2 package")
+    message(STATUS "Found system SDL2")
   endif()
 
+  # Add SDL2 link targets regardless of how it was obtained
+  list(APPEND GBE_DEPS_LINK
+    $<TARGET_NAME_IF_EXISTS:SDL2::SDL2main>
+    $<IF:$<TARGET_EXISTS:SDL2::SDL2>,SDL2::SDL2,SDL2::SDL2-static>
+  )
 endif()
-
--- cmake/deps/sdl_ttf.cmake
+++ cmake/deps/sdl_ttf.cmake
@@ -1,29 +1,35 @@
 if(NOT DEFINED EMSCRIPTEN)
-  string(TIMESTAMP BEFORE "%s")
-  CPMAddPackage(
+  find_package(SDL2_ttf QUIET)
+
+  if(NOT SDL2_ttf_FOUND)
+    message(STATUS "System SDL2_ttf not found, fetching using CPM...")
+    string(TIMESTAMP BEFORE "%s")
+
+    CPMAddPackage(
       NAME SDL_ttf
       GITHUB_REPOSITORY libsdl-org/SDL_ttf
       GIT_TAG release-2.24.0
       OPTIONS
-      "SDL_EXAMPLES OFF"
-      "SDL_SHARED OFF"
-      "SDL_STATIC ON"
-      "SDL_STATIC_PIC ON"
-      "SDL_WERROR OFF"
-  )
-
-  if(SDL_ttf_ADDED)
-      list(APPEND LEVE_DEPS_LINK
-      $<IF:$<TARGET_EXISTS:SDL2_ttf::SDL2_ttf>,SDL2_ttf::SDL2_ttf,SDL2_ttf::SDL2_ttf-static>
+        "SDL_EXAMPLES OFF"
+        "SDL_SHARED OFF"
+        "SDL_STATIC ON"
+        "SDL_STATIC_PIC ON"
+        "SDL_WERROR OFF"
     )
+
+    if(NOT SDL_ttf_ADDED)
+      message(FATAL_ERROR "Failed to add SDL2_ttf package")
+    endif()
 
     string(TIMESTAMP AFTER "%s")
     math(EXPR DELTASDL "${AFTER} - ${BEFORE}")
-    message(STATUS "SDL2_ttf TIME: ${DELTASDL}s")
+    message(STATUS "SDL2_ttf built from source in ${DELTASDL}s")
   else()
-    message(FATAL_ERROR "Failed to add SDL2_ttf package")
+    message(STATUS "Found system SDL2_ttf")
   endif()
 
+  # Add SDL2_ttf link target regardless of how it was obtained
+  list(APPEND LEVE_DEPS_LINK
+    $<IF:$<TARGET_EXISTS:SDL2_ttf::SDL2_ttf>,SDL2_ttf::SDL2_ttf,SDL2_ttf::SDL2_ttf-static>
+  )
 endif()
-
-
