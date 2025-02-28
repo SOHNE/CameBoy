81710bfdedecf5f44ea3e3bd0bd6fb7b78b8ff50
Leandro Peres <leandroperes@protonmail.com>
2025-02-28 10:50:36 -0300

    feat: Create 'instruction' module

---
+++ src/instr.c
@@ -0,0 +1,39 @@
+#include "camecore/camecore.h"
+#include "camecore/utils.h"
+
+typedef struct instruction
+{
+    InsType  type;
+    AddrMode mode;
+    RegType  reg_1;
+    RegType  reg_2;
+    CondType cond;
+    u8       param;
+} instruction;
+
+instruction instructions[0x100] = {
+    [0x00] = { INS_NOP, AM_IMP },
+
+    [0x05] = { INS_DEC, AM_R, RT_B },
+
+    [0x0E] = { INS_LD, AM_R_D8, RT_C },
+
+    [0xAF] = { INS_XOR, AM_R, RT_A },
+
+    [0xC3] = { INS_JP, AM_D16 },
+
+    [0xF3] = { INS_DI }
+};
+
+char * inst_lookup[] = { "<NONE>", "NOP",     "LD",      "INC",      "DEC",     "RLCA",    "ADD",     "RRCA",
+                         "STOP",   "RLA",     "JR",      "RRA",      "DAA",     "CPL",     "SCF",     "CCF",
+                         "HALT",   "ADC",     "SUB",     "SBC",      "AND",     "XOR",     "OR",      "CP",
+                         "POP",    "JP",      "PUSH",    "RET",      "CB",      "CALL",    "RETI",    "LDH",
+                         "JPHL",   "DI",      "EI",      "RST",      "INS_ERR", "INS_RLC", "INS_RRC", "INS_RL",
+                         "INS_RR", "INS_SLA", "INS_SRA", "INS_SWAP", "INS_SRL", "INS_BIT", "INS_RES", "INS_SET" };
+
+char *
+GetInstructionName( InsType t )
+{
+    return inst_lookup[t];
+}
