79a60f4536c60ea54baca78538d02fc90de1f733
Leandro Peres <leandroperes@protonmail.com>
2025-02-28 18:32:35 -0300

    feat: Implement CPU init

--- src/core.c
+++ src/core.c
@@ -1,8 +1,20 @@
 #include "camecore/camecore.h"
 #include "camecore/utils.h"
 
+typedef struct EmuContext
+{
+    bool paused;
+    bool running;
+    bool die;
+    u64  ticks;
+} EmuContext;
+
+extern void CPUInit( void );
+
 void
 Init( void )
 {
     LOG( LOG_INFO, "Initializing CameCore %s", CAMECORE_VERSION );
+
+    CPUInit();
 }
