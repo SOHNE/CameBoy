d00d88628d2e4a01a68d35b82a31d5eb35538755
Leandro Peres <leandroperes@protonmail.com>
2025-03-15 11:06:07 -0300

    build: Add package-lock

---
+++ package-lock.cmake
@@ -0,0 +1,24 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# PackageProject.cmake
+CPMDeclarePackage(PackageProject.cmake
+  VERSION 1.13.0
+  GITHUB_REPOSITORY TheLartians/PackageProject.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# GroupSourcesByFolder.cmake
+CPMDeclarePackage(GroupSourcesByFolder.cmake
+  VERSION 1.0
+  GITHUB_REPOSITORY TheLartians/GroupSourcesByFolder.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# CPMLicenses.cmake
+CPMDeclarePackage(CPMLicenses.cmake
+  VERSION 0.0.7
+  GITHUB_REPOSITORY cpm-cmake/CPMLicenses.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
